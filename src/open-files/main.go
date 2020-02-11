72d00c451a3902b1799605fe5432ac461fec6264
Sam Mirza <smirza@pivotal.io>
2020-02-11 11:06:42 -0800

    Make code more flexible
    Co-authored-by: Sam Mirza <smirza@pivotal.io>
    Co-authored-by: Daniel Keeney <dkeeney@pivotal.io>

--- src/open-files/main.go
+++ src/open-files/main.go
@@ -1,18 +1,34 @@
 package main
-
 import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strconv"
 )
-
-const MAX int = 100000
-
+var MAX int = 1000
+var SHARED_DIR string = "/shared"
 func main() {
+	argsWithoutProg := os.Args[1:]
+
+	if len(argsWithoutProg) < 2 {
+		var err error
+		MAX, err = strconv.Atoi(argsWithoutProg[0])
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		var err error
+		MAX, err = strconv.Atoi(argsWithoutProg[0])
+		if err != nil {
+			panic(err)
+		}
+		SHARED_DIR = argsWithoutProg[1]
+	}
+
 	var files = make([]*os.File, MAX)
 	defer closeFiles(files)
 	for i := 0; i < MAX; i++ {
-		file, err := ioutil.TempFile("", "ulimit-test-")
+		file, err := ioutil.TempFile(SHARED_DIR, "ulimit-test-")
 		if err != nil {
 			fmt.Println(err)
 			fmt.Printf("Failed to open file %d, see error above\n", i)
@@ -21,7 +37,6 @@ func main() {
 		files[i] = file
 	}
 }
-
 func closeFiles(files []*os.File) {
 	for i := 0; i < len(files); i++ {
 		if files[i] != nil {
